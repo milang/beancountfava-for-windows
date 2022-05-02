25c06cc1291ffa08b9e335fae87f6bab2b8505f0
Milan Gardian <mgardian@gmail.com>
2022-05-02 09:40:56 -0600

    Switch to a PowerShell file

---
+++ Build.ps1
@@ -0,0 +1,13 @@
+﻿#Requires -version 7
+
+Write-Host -NoNewline "Current directory: "
+Write-Host -ForegroundColor Yellow (Get-Location)
+
+Get-Command git
+git --version
+
+Get-Command python
+python --version
+
+Get-Command pip
+pip --version
